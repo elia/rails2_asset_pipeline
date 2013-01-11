62f57fb51eb9ec8832147afc966d0e7aa770b4ee
Elia Schito <elia@schito.me>
2013-01-11 11:09:26 +0100

    Update RSpec and add good config defaults

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,16 @@ RSpec.configure do |config|
     Rails2AssetPipeline.manifest = nil
     Rails2AssetPipeline.prefix = nil
   end
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
 end
 
 def run(cmd)
